cdd04ba2c3c2ed9f387f5c84cd2064949077b408
Taha Ahmed Mallick <tahaahmedmallick@gmail.com>
2023-02-19 17:27:31 +0500

    few changes to mouse pointer

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 let playing = false;
 
-let cursor = document.getElementsByClassName('cursor');
-let strtBtn = document.getElementsByClassName('strt-btn')[0];
-let page = document.getElementsByClassName('page');
-let cvs = document.getElementsByTagName('canvas')[0];
-let barier = document.querySelector("div.barier");
-let barierθ = document.querySelector("div.barier>div.target");
-let line = document.getElementsByClassName('line')[0];
-let enemyGround = document.getElementsByClassName('enemies')[0];
-let ctx = cvs.getContext('2d');
+const cursor = document.getElementsByClassName('cursor');
+const strtBtn = document.getElementsByClassName('strt-btn')[0];
+const page = document.getElementsByClassName('page');
+const cvs = document.getElementsByTagName('canvas')[0];
+const barier = document.querySelector("div.barier");
+const barierθ = document.querySelector("div.barier>div.target");
+const line = document.getElementsByClassName('line')[0];
+const ctx = cvs.getContext('2d');
+
 let widthW = window.innerWidth;
 let heightW = window.innerHeight;
-let angleDeg, angleRad, t = 5000, qty = 1, indexE = 0;
+let angleDeg, angleRad, time = 5000, qty = 1;
 
-let target = new Image();
-target.src = './resources/crosshair.png';
-let targetClick = new Image();
-targetClick.src = './resources/crosshair-click.png';
+let mousePos = {
+      x: 0,
+      y: 0
+};
 
 cvs.height = heightW;
 cvs.width = widthW;
@@ -29,16 +29,19 @@ window.addEventListener('resize', () => {
 })
 
 window.addEventListener('mousemove', eve => {
-      let mouseX = eve.clientX;
-      let mouseY = eve.clientY;
-      cursor[0].style.top = mouseY + "px";
-      cursor[0].style.left = mouseX + "px";
-      if (playing) {
+      if (!playing) {
+            mousePos.x = eve.clientX;
+            mousePos.y = eve.clientY;
+            cursor[0].style.top = mousePos.y + "px";
+            cursor[0].style.left = mousePos.x + "px";
+      } else if (playing) {
+            mousePos.x += eve.movementX;
+            mousePos.y += eve.movementY;
             let rect = barier.getBoundingClientRect();
             let barierX = rect.left + rect.width / 2;
             let barierY = rect.top + rect.height / 2;
 
-            let θdeg = angle(mouseX, mouseY, barierX, barierY);
+            let θdeg = angle(mousePos.x, mousePos.y, barierX, barierY);
             barierθ.style.transform = `rotate(${360 - θdeg}deg)`;
       }
 });
@@ -55,8 +58,19 @@ function angle(cx, cy, ex, ey) {
       return angleθ;
 }
 
-window.addEventListener('mousedown', e => { cursor[0].src = './resources/crosshair-click.png'; cursor[1].src = './resources/crosshair-click.png'; });
-window.addEventListener('mouseup', e => { cursor[0].src = './resources/crosshair.png'; cursor[1].src = './resources/crosshair.png'; });
+window.addEventListener('mousedown', e => {
+      cursor[0].src = './resources/crosshair-click.png';
+      cursor[1].src = './resources/crosshair-click.png';
+      if (playing) {
+            line.classList.add('fire-anime');
+            setTimeout(() => line.classList.remove('fire-anime'), 200);
+            fire.push(new Fire(angleDeg, angleRad, widthW / 2, heightW / 2));
+      }
+});
+window.addEventListener('mouseup', e => {
+      cursor[0].src = './resources/crosshair.png';
+      cursor[1].src = './resources/crosshair.png';
+});
 
 strtBtn.addEventListener('click', () => {
       page[0].style.display = "none";
@@ -65,18 +79,11 @@ strtBtn.addEventListener('click', () => {
       cursor[0].style.display = "none";
       playing = true;
       // document.documentElement.requestFullscreen();
+      document.body.requestPointerLock();
 });
 
 let fireSpeed = [5, 6, 3, 7];
 let fire = [];
-
-document.getElementsByClassName("game")[0].addEventListener('click', () => {
-      if (playing) {
-            line.classList.add('fire-anime');
-            setTimeout(() => line.classList.remove('fire-anime'), 200);
-            fire.push(new Fire(angleDeg, angleRad, widthW / 2, heightW / 2));
-      }
-});
 
 class Fire {
       constructor(angleDeg, angleRad, x, y) {
@@ -99,9 +106,6 @@ class Fire {
       create() {
             ctx.translate(this.sx, this.sy);
             ctx.rotate(this.angleDegM * Math.PI / 180);
-            // ctx.arc(this.x, this.y, 5, 0, Math.PI * 2, true);
-            // ctx.fillStyle = '#f44336';
-            // ctx.fill();
             ctx.fillStyle = '#f44336';
             ctx.fillRect(this.x - 5, this.y - 4, 10, 7);
             ctx.fillStyle = '#ffc107';
@@ -156,56 +160,69 @@ class Enemy {
       }
 
       spawn() {
-            enemyGround.innerHTML += `
-            <div class="tank enemy" id="${indexE}">
-                  <div class="circle enemy">    
-                        <div class="line enemy"></div>
-                  </div>
-                  <div class="tank-body enemy"></div>
-            </div>`;
-            this.enemy = document.getElementById(`${indexE}`);
-            indexE++;
-            this.enemy.style.top = this.y + "px";
-            this.enemy.style.left = this.x + "px"
-      }
+            ctx.beginPath();
+            ctx.fillStyle = "#800d00";
+            ctx.fillRect(this.x - 70 / 2, this.y - 56 / 2, 70, 56);
+            ctx.lineWidth = 2.5;
+            ctx.strokeStyle = "#303030";
+            ctx.strokeRect(this.x - 70 / 2, this.y - 56 / 2, 70, 56);
+            ctx.closePath();
 
-      rotate() {
-            let r = this.enemy.childNodes[1];
-            console.log(r);
-            r.style.transform = `translate(-50%, -50%) scale(0.85) rotate(${++this.angle}deg)`;
+            // linear gradient
+            ctx.beginPath();
+            let linearGrad = ctx.createLinearGradient(this.x, this.y - 6, 0, this.y + 40);
+            linearGrad.addColorStop(0, "#920101");
+            linearGrad.addColorStop(0.85, "#ff3801");
+            ctx.fillStyle = linearGrad;
+            ctx.fillRect(this.x + 16, this.y - 6, 40, 12);
+            ctx.strokeRect(this.x + 16, this.y - 6, 40, 12);
+            ctx.closePath();
+
+            // radial gradient
+            let radGrad = ctx.createRadialGradient(this.x, this.y, 0, this.x, this.y, 21.25);
+            radGrad.addColorStop(0, "#ff3801")
+            radGrad.addColorStop(1, "#2d0702");
+
+            // circle
+            ctx.beginPath();
+            ctx.fillStyle = radGrad;
+            ctx.arc(this.x, this.y, 21.25, 0, Math.PI * 2);
+            ctx.stroke();
+            ctx.fill();
+            ctx.closePath();
       }
 }
 
-// function animation() {
-//       ctx.clearRect(0, 0, widthW, heightW);
-//       for (let i = 0; i < fire.length; i++) {
-//             if (fire[i].left <= widthW * 1.25) {
-//                   fire[i].move();
-//             } else {
-//                   fire.splice(i, 1);
-//             }
-//       }
-//       for (let i = 0; i < enemies.length; i++) {
-//             enemies[i].rotate();
-//       }
-//       requestAnimationFrame(animation);
-// }
+function animation() {
+      ctx.clearRect(0, 0, widthW, heightW);
+      for (let i = 0; i < fire.length; i++) {
+            if (fire[i].left <= widthW * 1.25) {
+                  fire[i].move();
+            } else {
+                  fire.splice(i, 1);
+            }
+      }
+      for (let i = 0; i < enemies.length; i++) {
+            enemies[i].spawn();
+      }
+      requestAnimationFrame(animation);
+}
 
-// animation();
+animation();
 
 
-
-// setInterval(() => {
-//       if (playing) {
-//             for (let i = 0; i < qty; i++) {
-//                   let Ex = Math.random() * widthW;
-//                   let Ey = Math.random() * heightW;
-//                   Ex < widthW / 2 ? Ex + 50 : Ex - 50;
-//                   Ey < heightW / 2 ? Ey + 50 : Ey - 50;
-//                   enemies.push(new Enemy(Ex, Ey, fireSpeed[Math.floor(Math.random() * fireSpeed.length)]));
-//             }
-//       }
-// }, t);
+// Enemies spawner
+setInterval(() => {
+      if (playing) {
+            for (let i = 0; i < qty; i++) {
+                  let Ex = Math.random() * widthW;
+                  let Ey = Math.random() * heightW;
+                  Ex < widthW / 2 ? Ex + 50 : Ex - 50;
+                  Ey < heightW / 2 ? Ey + 50 : Ey - 50;
+                  enemies.push(new Enemy(Ex, Ey, fireSpeed[Math.floor(Math.random() * fireSpeed.length)]));
+            }
+      }
+}, time);
 
 // bullet
 // ctx.arc(300, 300, 5, 0, Math.PI * 2, true);
@@ -218,12 +235,22 @@ class Enemy {
 // ctx.fillStyle = '#ffeb3b';
 // ctx.fillRect(100, 322, 10, 7);
 
+// radial gradient
 // grad = ctx.createRadialGradient(300, 300, 0, 300, 300, 21.25)
 // grad.addColorStop(0, "#ff3801")
 // grad.addColorStop(1, "#2d0702");
+
+// circle
 // ctx.fillStyle = grad;
 // ctx.lineWidth = 2.5;
 // ctx.strokeStyle = "#303030";
 // ctx.arc(300, 300, 21.25, 0, Math.PI * 2);
 // ctx.stroke();
 // ctx.fill();
+
+// linear gradient
+// grad = ctx.createLinearGradient(0, 0, 40, 0)
+// grad.addColorStop(0, "#920101")
+// grad.addColorStop(1, "#ff3801")
+// ctx.fillStyle = grad
+// ctx.fillRect(0, 0, 40, 12)
