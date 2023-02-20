04549fea2e9eca24aa79cf6601b25b4b25543017
Taha Ahmed Mallick <tahaahmedmallick@gmail.com>
2023-02-20 17:30:12 +0500

    A very big problem solved 'left -> screenX'

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-let playing = false;
-
 const cursor = document.getElementsByClassName('cursor');
 const strtBtn = document.getElementsByClassName('strt-btn')[0];
 const page = document.getElementsByClassName('page');
@@ -9,25 +7,30 @@ const barierθ = document.querySelector("div.barier>div.target");
 const line = document.getElementsByClassName('line')[0];
 const ctx = cvs.getContext('2d');
 
-let widthW = window.innerWidth;
-let heightW = window.innerHeight;
-let angleDeg, angleRad, time = 5000, qty = 1;
+class Dimensions {
+      constructor() {
+            this.height = window.innerHeight;
+            this.width = window.innerWidth;
+            this.diagonal = Math.sqrt(this.height ** 2 + this.width ** 2);
+            this.function();
+      }
+      function() {
+            this.height = window.innerHeight;
+            this.width = window.innerWidth;
+            this.diagonal = Math.sqrt(this.height ** 2 + this.width ** 2);
+            cvs.height = this.height;
+            cvs.width = this.width;
+      }
+}
+let dimensions = new Dimensions;
+window.addEventListener('resize', () => dimensions.function());
+
+let angleDeg, angleRad, time = 5000, qty = 1, playing = false;
 
 let mousePos = {
       x: 0,
       y: 0
 };
-
-cvs.height = heightW;
-cvs.width = widthW;
-
-window.addEventListener('resize', () => {
-      widthW = window.innerWidth;
-      heightW = window.innerHeight;
-      cvs.height = heightW;
-      cvs.width = widthW;
-})
-
 window.addEventListener('mousemove', eve => {
       if (!playing) {
             mousePos.x = eve.clientX;
@@ -54,20 +57,19 @@ function angle(cx, cy, ex, ey) {
       sinθ = Math.sin(angleRad);
       cosθ = Math.cos(angleRad);
       angleDeg = angleθ;
-      console.log(angleDeg, sinθ, cosθ);
       return angleθ;
 }
 
-window.addEventListener('mousedown', e => {
+window.addEventListener('mousedown', () => {
       cursor[0].src = './resources/crosshair-click.png';
       cursor[1].src = './resources/crosshair-click.png';
       if (playing) {
             line.classList.add('fire-anime');
             setTimeout(() => line.classList.remove('fire-anime'), 200);
-            fire.push(new Fire(angleDeg, angleRad, widthW / 2, heightW / 2));
+            fire.push(new Fire(angleDeg, angleRad, dimensions.width / 2, dimensions.height / 2));
       }
 });
-window.addEventListener('mouseup', e => {
+window.addEventListener('mouseup', () => {
       cursor[0].src = './resources/crosshair.png';
       cursor[1].src = './resources/crosshair.png';
 });
@@ -78,7 +80,6 @@ strtBtn.addEventListener('click', () => {
       page[1].style.zIndex = "10";
       cursor[0].style.display = "none";
       playing = true;
-      // document.documentElement.requestFullscreen();
       document.body.requestPointerLock();
 });
 
@@ -88,23 +89,23 @@ let fire = [];
 class Fire {
       constructor(angleDeg, angleRad, x, y) {
             this.angleDeg = angleDeg;
-            this.angleDegM = 90 - angleDeg
+            this.angleDegM = 90 - angleDeg;
             this.angleRad = angleRad;
             this.cosθ = Math.cos(angleRad);
             this.sinθ = Math.sin(angleRad);
             this.speed = fireSpeed[Math.floor(Math.random() * fireSpeed.length)];
             this.y = 0;
             this.x = 0;
-            this.sx = x;
-            this.sy = y;
+            this.sourceX = x;
+            this.sourceY = y;
             this.k = this.cosθ / this.sinθ;
-            this.left = window.innerWidth + this.x;
-            this.top = window.innerHeight + this.y;
+            this.screenX = dimensions.width + this.x;
+            this.screenY = dimensions.height + this.y;
             this.create();
       }
 
       create() {
-            ctx.translate(this.sx, this.sy);
+            ctx.translate(this.sourceX, this.sourceY);
             ctx.rotate(this.angleDegM * Math.PI / 180);
             ctx.fillStyle = '#f44336';
             ctx.fillRect(this.x - 5, this.y - 4, 10, 7);
@@ -115,20 +116,19 @@ class Fire {
             ctx.closePath();
             console.log(this.angleDegM, this.sinθ, this.cosθ);
             ctx.rotate(Math.PI * 2 - this.angleDegM * Math.PI / 180);
-            ctx.translate(-this.sx, -this.sy);
-            this.left = widthW / 2 + this.x;
-            this.top = heightW / 2 + this.y;
+            ctx.translate(-this.sourceX, -this.sourceY);
+            this.screenX = dimensions.width / 2 + this.x;
+            this.screenY = dimensions.height / 2 + this.y;
       }
 
       move() {
 
-            ctx.translate(this.sx, this.sy);
+            ctx.translate(this.sourceX, this.sourceY);
             ctx.rotate(this.angleDegM * Math.PI / 180);
 
             this.x -= this.speed;
             this.y = this.x / this.k;
 
-            console.log("Working");
             ctx.fillStyle = '#f44336';
             ctx.fillRect(-5, this.x - 4, 10, 7);
             ctx.fillStyle = '#ffc107';
@@ -138,11 +138,9 @@ class Fire {
             ctx.closePath();
 
             ctx.rotate(Math.PI * 2 - this.angleDegM * Math.PI / 180);
-            ctx.translate(-this.sx, -this.sy);
-            this.left = widthW / 2 - this.x;
-            this.top = heightW / 2 + this.y;
-            console.log(this.left, this.top);
-            console.log(this.x, this.y);
+            ctx.translate(-this.sourceX, -this.sourceY);
+            this.screenX = dimensions.width / 2 - this.x;
+            this.screenY = dimensions.height / 2 + this.y;
       }
 }
 
@@ -194,9 +192,10 @@ class Enemy {
 }
 
 function animation() {
-      ctx.clearRect(0, 0, widthW, heightW);
+      ctx.clearRect(0, 0, dimensions.width, dimensions.height);
       for (let i = 0; i < fire.length; i++) {
-            if (fire[i].left <= widthW * 1.25) {
+            console.table(fire);
+            if (fire[i].screenX <= dimensions.diagonal) {
                   fire[i].move();
             } else {
                   fire.splice(i, 1);
@@ -210,15 +209,14 @@ function animation() {
 
 animation();
 
-
 // Enemies spawner
 setInterval(() => {
       if (playing) {
             for (let i = 0; i < qty; i++) {
-                  let Ex = Math.random() * widthW;
-                  let Ey = Math.random() * heightW;
-                  Ex < widthW / 2 ? Ex + 50 : Ex - 50;
-                  Ey < heightW / 2 ? Ey + 50 : Ey - 50;
+                  let Ex = Math.random() * dimensions.width;
+                  let Ey = Math.random() * dimensions.height;
+                  Ex < dimensions.width / 2 ? Ex + 50 : Ex - 50;
+                  Ey < dimensions.height / 2 ? Ey + 50 : Ey - 50;
                   enemies.push(new Enemy(Ex, Ey, fireSpeed[Math.floor(Math.random() * fireSpeed.length)]));
             }
       }
